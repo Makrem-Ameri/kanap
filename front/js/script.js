2c571191154a160d30efbc1df211dcbe39feb15f
Ameri <130809050+Makrem-Ameri@users.noreply.github.com>
2023-09-22 18:16:36 +0200

    Update script.js
    changer l'affichage liste de produit en utilisant les balises html

--- front/js/script.js
+++ front/js/script.js
@@ -1,29 +1,25 @@
-fetch("http://localhost:3000/api/products")
-    .then(res => res.json()) // On renvoi une réponse en promesse traité en JSON
-    .then(mappedProducts => { // On définit un paramètre pour mappedProducts en réutilisant .then
-        for (let product of mappedProducts) {
-            let kanap_id = document.createElement("a");
-            let kanapImg = document.createElement("img");
-            let kanapName = document.createElement("h3");
-            let kanapAltText = document.createElement("alt")
-            let kanapPrice = document.createElement("p");
-            let kanapDescription = document.createElement("p");
-            let kanapElement = document.createElement("article");
-
-            kanap_id.href = ("./html/product.html?id=" + product._id);
-            kanapImg.src = product.imageUrl;
-            kanapName.innerText = product.name;
-            kanapAltText.alt = product.name;
-            kanapPrice.textContent = "Prix : " + product.price + " €";
-            kanapDescription.textContent = product.description;
-
-            kanap_id.appendChild(kanapElement);
-            kanapElement.appendChild(kanapImg);
-            kanapElement.appendChild(kanapName);
-            kanapElement.appendChild(kanapAltText);
-            kanapElement.appendChild(kanapPrice);
-            kanapElement.appendChild(kanapDescription);
-
-            document.getElementById("items").appendChild(kanap_id);
-        }
-    })
+fetch('http://localhost:3000/api/products')
+.then((response)=>response.json())
+.then((data)=>{
+    displayProduct(data);
+})
+.catch((error)=>{
+    console.log(error);
+})
+//fonction afficher la liste des produits
+function displayProduct(products){
+    for(product of products){
+        const products=`
+        <a href="./product.html?id=${product._id}">
+        <article>
+          <img src="${product.imageUrl}" alt="${product.altTxt}">
+          <h3 class="productName">${product.name}</h3>
+          <p class="productDescription">${product.description}</p>
+        </article>
+      </a>
+      `;
+      document.getElementById('items')
+      .insertAdjacentHTML('beforeend',products)
+    }
+  } 
+  
