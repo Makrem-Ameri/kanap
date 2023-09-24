297e2e63af167646bedda297df46492c8b9bb943
Ameri <130809050+Makrem-Ameri@users.noreply.github.com>
2023-09-24 23:58:05 +0200

    Update confirmation.js
    corriger ligne de code.

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,20 +1,4 @@
+document.getElementById('orderId').innerHTML = new URLSearchParams(
+  location.search
+).get('orderId');
 
-const orderId = getOrderId()
-displayOrderId(orderId)
-removeAllCache()
-
-function getOrderId() {
-  const queryString = window.location.search
-  const urlParams = new URLSearchParams(queryString)
-  return urlParams.get("orderId")
-}
-
-function displayOrderId(orderId) {
-  const orderIdElement = document.getElementById("orderId")
-  orderIdElement.textContent = orderId
-}
-
-function removeAllCache() {
-  const cache = window.localStorage
-  cache.clear()
-}
